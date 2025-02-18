1609fa594ad9ed36c0cb310e2aa269c26a8281a1
Georgi Ganchev <googs.bs@gmail.com>
2025-02-18 21:12:24 +0200

    feat: add bookings crud

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+class BookingsController < ApplicationController
+
+  before_action :set_property
+  before_action :set_room
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  # GET /properties/:property_id/rooms/:room_id/bookings
+  def index
+    @bookings = @room.bookings
+  end
+
+  # GET /properties/:property_id/rooms/:room_id/bookings/:id
+  def show
+  end
+
+  # GET /properties/:property_id/rooms/:room_id/bookings/new
+  def new
+    @booking = @room.bookings.build
+  end
+
+  # GET /properties/:property_id/rooms/:room_id/bookings/:id/edit
+  def edit
+  end
+
+  # POST /properties/:property_id/rooms/:room_id/bookings
+  def create
+    @booking = @room.bookings.build(booking_params)
+
+    if @booking.save
+      redirect_to property_room_booking_path(@property, @room, @booking),
+                  notice: t("flash.bookings.created_successfully")
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /properties/:property_id/rooms/:room_id/bookings/:id
+  def update
+    if @booking.update(booking_params)
+      redirect_to property_room_booking_path(@property, @room, @booking),
+                  notice: t("flash.bookings.updated_successfully"), status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /properties/:property_id/rooms/:room_id/bookings/:id
+  def destroy
+    @booking.destroy!
+    redirect_to property_room_bookings_path(@property, @room), notice: t("flash.bookings.destroyed_successfully"),
+                                                               status: :see_other
+  end
+
+  private
+
+  def set_property
+    @property = Current.account.properties.find(params[:property_id])
+  end
+
+  def set_room
+    @room = @property.rooms.find(params[:room_id])
+  end
+
+  def set_booking
+    @booking = @room.bookings.find(params[:id])
+  end
+
+  def booking_params
+    params.expect(booking: [:adults, :children, :deposit, :notes])
+  end
+
+end
---
+++ test/controllers/bookings_controller_test.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class BookingsControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @user = users(:one)
+    @property = properties(:one)
+    @room = rooms(:one)
+    @booking = bookings(:one)
+
+    @room.bookings << @booking
+
+    set_current_user(@user)
+  end
+
+  test "should get index" do
+    get property_room_bookings_url(@property, @room)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_property_room_booking_url(@property, @room)
+    assert_response :success
+  end
+
+  test "should create booking" do
+    assert_difference("Booking.count", 1) do
+      post property_room_bookings_url(@property, @room), params: {
+        booking: {
+          adults: @booking.adults,
+          children: @booking.children,
+          deposit: @booking.deposit,
+          notes: @booking.notes
+        }
+      }
+    end
+
+    assert_redirected_to property_room_booking_url(@property, @room, Booking.last)
+  end
+
+  test "should show booking" do
+    get property_room_booking_url(@property, @room, @booking)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_property_room_booking_url(@property, @room, @booking)
+    assert_response :success
+  end
+
+  test "should update booking" do
+    patch property_room_booking_url(@property, @room, @booking), params: {
+      booking: {
+        adults: @booking.adults,
+        children: @booking.children,
+        deposit: @booking.deposit,
+        notes: @booking.notes
+      }
+    }
+    assert_redirected_to property_room_booking_url(@property, @room, @booking)
+  end
+
+  test "should destroy booking" do
+    assert_difference("Booking.count", -1) do
+      delete property_room_booking_url(@property, @room, @booking)
+    end
+
+    assert_redirected_to property_room_bookings_url(@property, @room)
+  end
+
+end
