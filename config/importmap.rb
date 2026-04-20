e7e724d78d726a7efe2e3082b033486518a55219
Georgi Ganchev <googs.bs@gmail.com>
2026-04-20 17:58:18 +0100

    chore: update lodash

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "lodash" # @4.17.23
+pin "lodash" # @4.18.1
 pin_all_from "app/javascript/controllers", under: "controllers"
